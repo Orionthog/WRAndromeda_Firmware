b906c3ee5356fcd8d079d90e47bfd2299ddf72ee
Igor Santa Bárbara <igor.barbara@wr.sc.usp.br>
2018-04-25 13:12:45 -0300

    preparing stuff to start programming

---
+++ firmware/WRAndromeda.X/System.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   System.h
+ * Author: Igor
+ *
+ * Created on 24 de Abril de 2018, 09:26
+ */
+
+#ifndef SYSTEM_H
+#define	SYSTEM_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* SYSTEM_H */
+
---
+++ firmware/WRAndromeda.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=WRAndromeda.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/WRAndromeda.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=wrandromeda.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/wrandromeda.x.tar
